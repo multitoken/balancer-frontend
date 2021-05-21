6976f3d0b68a16fe9ad54da4e9e1f3e10dde5b83
Alex O <maightycron@gmail.com>
2021-05-21 17:39:00 +0500

    poolTokens for kovan

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -19,6 +19,47 @@ interface Token {
     logoURI?: string;
 }
 
+const poolTokens = new Set([
+    '0x266a9aac60b0211d7269dd8b0e792d645d2923e6',
+    '0x8b8cfa2f2411839c7c29089913a9734637a26383',
+    '0x8c4c3c9b503700431b25840f26cef3896774e913',
+    '0x7f0af1a00aa20dd127019ddb79957c6270068b64',
+    '0x74a2affdbb8caf17f5ca650bc9303a559ac24b24',
+    '0xfd74354313b7a8cfc59f779f6ba5b0053e75a16b',
+    '0x6412578d9eb222964bb5fcf2eeb0f7aee09fe629',
+    '0x6a86df5759f86e4d69a474c90f4089be2da87950',
+    '0xa98e1c95890f03723662cbf0809eeb17d7d5b346',
+    '0x5bba5f0046836c43164b75c5148a3a4253479355',
+    '0xc33bdb953b982f7c3010f908fef32d0b251d6b29',
+    '0x7dad0d1c1a012b8ab5f7c2fb93469440726fe7e5',
+    '0x643f466dde4f53fe3948606ff64155cc02decce0',
+    '0xc9e5545a6cc902598573b81569cf634962681099',
+    '0xc175c8994a738616255e1a8ba6278d9341199994',
+    '0x019137bd6a9a9b2a6d0bfa302e48cd6b2db5086d',
+    '0x53f3efcf31bf3227d8ad65ade5197cc8852e99da',
+    '0x22bf885bf1c9cc6a12d66929a8c22cc95acfb5cd',
+    '0x010db7b4054d29a628a64493e49364b70a7a2a66',
+    '0xec2e1ef466d53d16e70dee0b0bcbc7020f8e58c5',
+    '0x266a9aac60b0211d7269dd8b0e792d645d2923e6',
+    '0x8b8cfa2f2411839c7c29089913a9734637a26383',
+    '0x8c4c3c9b503700431b25840f26cef3896774e913',
+    '0x7f0af1a00aa20dd127019ddb79957c6270068b64',
+    '0x74a2affdbb8caf17f5ca650bc9303a559ac24b24',
+    '0xfd74354313b7a8cfc59f779f6ba5b0053e75a16b',
+    '0x6412578d9eb222964bb5fcf2eeb0f7aee09fe629',
+    '0x6a86df5759f86e4d69a474c90f4089be2da87950',
+    '0xa98e1c95890f03723662cbf0809eeb17d7d5b346',
+    '0x5bba5f0046836c43164b75c5148a3a4253479355',
+    '0xc33bdb953b982f7c3010f908fef32d0b251d6b29',
+    '0x7dad0d1c1a012b8ab5f7c2fb93469440726fe7e5',
+    '0x643f466dde4f53fe3948606ff64155cc02decce0',
+    '0xc9e5545a6cc902598573b81569cf634962681099',
+    '0xc175c8994a738616255e1a8ba6278d9341199994',
+    '0x019137bd6a9a9b2a6d0bfa302e48cd6b2db5086d',
+    '0x53f3efcf31bf3227d8ad65ade5197cc8852e99da',
+    '0x22bf885bf1c9cc6a12d66929a8c22cc95acfb5cd',
+]);
+
 export const DEFAULT_LIST = 'balancer';
 
 export const listMetadata: Record<string, string> = {
@@ -32,6 +73,7 @@ export const listMetadata: Record<string, string> = {
 
 export async function getTokenlist(id: string): Promise<TokenList> {
     if (id === DEFAULT_LIST) {
+        tokenlist.tokens = tokenlist.tokens.filter(t => t.chainId != 42 || poolTokens.has(t.address.toLocaleLowerCase()));
         return tokenlist;
     }
     const listUrl = listMetadata[id];
